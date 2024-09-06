8dd3359c155ebeeb24a15a9ab11433f84c35a08c
nebal9 <nebal.9801@gmail.com>
2024-09-06 14:30:38 +0800

    location + images + timeout

--- server/api/formsite/createListing.js
+++ server/api/formsite/createListing.js
@@ -1,6 +1,16 @@
+const { default: axios } = require('axios');
+const fs = require('fs');
+const path = require('path');
+
 const { handleError } = require('../../api-util/sdk');
 const { types } = require('sharetribe-flex-sdk');
 const { LatLng } = types;
+
+const flexIntegrationSdk = require('sharetribe-flex-integration-sdk');
+const integrationSdk = flexIntegrationSdk.createInstance({
+    clientId: process.env.SHARETRIBE_INTEGRATION_CLIENT_ID,
+    clientSecret: process.env.SHARETRIBE_INTEGRATION_CLIENT_SECRET
+});
 
 const Mapper = {
     OwnerFirstName: '14',
@@ -24,14 +34,6 @@ const Mapper = {
 
 module.exports = async (req, res) => {
     try {
-        // const {
-        // FedExID,
-        // title,
-        // description,
-        // price,
-        // ...restOfFields
-        // } = req.body;
-
         const OwnerFirstName = req.body.items.find(i => i.id == Mapper["OwnerFirstName"]).value;
         const OwnerLastName = req.body.items.find(i => i.id == Mapper["OwnerLastName"]).value;
         const CompanyName = req.body.items.find(i => i.id == Mapper["CompanyName"]).value;
@@ -71,12 +73,6 @@ module.exports = async (req, res) => {
             TruckPictures,
         };
 
-        const flexIntegrationSdk = require('sharetribe-flex-integration-sdk');
-        const integrationSdk = flexIntegrationSdk.createInstance({
-            clientId: process.env.SHARETRIBE_INTEGRATION_CLIENT_ID,
-            clientSecret: process.env.SHARETRIBE_INTEGRATION_CLIENT_SECRET
-        });
-
         if (FedExID) {
             const foundUserRes = await integrationSdk.users.query({
                 pub_FedExID: Number(FedExID)
@@ -90,29 +86,45 @@ module.exports = async (req, res) => {
                 } = foundUser.attributes;
 
                 if (deleted || banned) {
-                    res.status(400)
-                        .send("User is either banned or deleted.")
+                    res.status(400).send("User is either banned or deleted.");
                 } else {
                     const notAllowedToPublish = permissions.postListings == "permission/deny";
                     const listingState = notAllowedToPublish ? "pendingApproval" : "published";
+
+                    res.status(200)
+                            .set('Content-Type', 'application/transit+json')
+                            .send({ success: true });
+
+                    const {
+                        country,
+                        postalCode,
+                        address,
+                        geoLocation,
+                    } = await getPlaceInfo(RentalLocation);
+
+                    const truckImagesURLs = TruckPictures.map(tp => tp.value);
+                    const images = await uploadImages(truckImagesURLs);
 
                     const params = {
                         title,
                         authorId: foundUser.id,
                         state: listingState,
-                        // geolocation: new LatLng(40.64542, -74.08508),
+                        geolocation: new LatLng(geoLocation.lat, geoLocation.lng),
                         publicData: {
-                            // address: {
-                            // city: "New York",
-                            // country: "USA",
-                            // state: "NY",
-                            // street: "230 Hamilton Ave"
-                            // },
+                            location: {
+                                address,
+                                building: "",
+                            },
+                            addressInfo: {
+                                country,
+                                postalCode,
+                            },
                             listingType: "daily-rental",
                             transactionProcessAlias: "default-booking/release-1",
                             unitType: "day",
                             ...publicDataItems,
                         },
+                        images: images.filter(image => image),
                     };
 
                     // if (description) {
@@ -128,12 +140,11 @@ module.exports = async (req, res) => {
 
                     try {
                         const listingRes = await integrationSdk.listings.create(params);
-
-                        res.status(200)
-                            .set('Content-Type', 'application/transit+json')
-                            .send(listingRes)
+                        // res.status(200)
+                        //     .set('Content-Type', 'application/transit+json')
+                        //     .send(listingRes)
                     } catch (error) {
-                        handleError(res, error);
+                        // handleError(res, error);
                     };
                 };
             } else {
@@ -142,8 +153,86 @@ module.exports = async (req, res) => {
         } else {
             res.status(400).send("'FedExID' was not provided.");
         };
-
     } catch (err) {
         handleError(res, err);
     };
+};
+
+
+const getAPI = async (url) => {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        return json;
+    } catch (error) {
+        console.error(error.message);
+    };
+};
+
+const getPlaceInfo = async (inputValue) => {
+    const GoogleMapsAPI = "AIzaSyDFSxIB7gNedlSKsX5GRLu807LXyi8KnUE";
+
+    const placeURL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${inputValue}&types=street_address&key=${GoogleMapsAPI}`;
+
+    const placeRes = await getAPI(placeURL);
+    const placeId = placeRes.predictions[0]?.place_id;
+
+    if (placeId) {
+        const infoURL = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GoogleMapsAPI}`;
+
+        const infoRes = await getAPI(infoURL);
+        const { address_components = [], formatted_address, geometry } = infoRes?.result || {};
+
+        const country = address_components.find(a => a.types.includes("country"))?.short_name;
+        const postalCode = address_components.find(a => a.types.includes("postal_code"))?.short_name;
+
+        return {
+            country,
+            postalCode,
+            address: formatted_address,
+            geoLocation: geometry.location,
+        };
+    };
+};
+
+const uploadImages = async (imgURLs) => {
+    const imagesUploadPromise = imgURLs.map(async (imageUrl, j) => {
+        const dirPath = './temp-photos';
+        if (!fs.existsSync(dirPath)) {
+            fs.mkdirSync(dirPath, { recursive: true });
+        };
+        const filePath = path.resolve(`${dirPath}/${new Date().toISOString().replace(/:/g, '-') + j}.jpg`);
+
+        const promise = new Promise(async (resolve, reject) => {
+            try {
+                var photoData = await axios({
+                    url: imageUrl,
+                    responseType: 'stream',
+                });
+                const writer = fs.createWriteStream(filePath);
+                photoData.data.pipe(writer);
+                writer.on('finish', async () => {
+                    try {
+                        const image = await integrationSdk.images.upload(
+                            { image: filePath },
+                            { expand: true },
+                        );
+                        fs.unlinkSync(filePath);
+                        return resolve(image?.data?.data?.id?.uuid);
+                    } catch (err) {
+                        console.log(err);
+                        fs.unlinkSync(filePath);
+                        return resolve();
+                    };
+                });
+            } catch (err) {
+                console.log(err);
+                return resolve();
+            };
+        });
+        return promise;
+    });
+
+    const images = await Promise.all(imagesUploadPromise);
+    return images;
 };
